82c20c06d13ed518cdb8c7610d06350e2c2754be
freewind <582105721@qq.com>
2018-01-04 17:30:39 +0800

    bug fix

--- domain.go
+++ domain.go
@@ -28,34 +28,34 @@ type Document struct {
 }
 
 type Scroll struct {
-	Took int `json:"took"`
+	Took     int    `json:"took"`
 	ScrollId string `json:"_scroll_id"`
 	TimedOut bool   `json:"timed_out"`
 	Hits     struct {
-		     MaxScore float32    `json:"max_score"`
-		     Total int           `json:"total"`
-		     Docs  []interface{} `json:"hits"`
-	     } `json:"hits"`
+		MaxScore float32       `json:"max_score"`
+		Total    int           `json:"total"`
+		Docs     []interface{} `json:"hits"`
+	} `json:"hits"`
 	Shards struct {
-		     Total int `json:"total"`
-		     Successful int `json:"successful"`
-		     Failed int `json:"failed"`
-		     Failures []struct {
-			     Shard int     `json:"shard"`
-			     Index string  `json:"index"`
-			     Status int    `json:"status"`
-			     Reason interface{} `json:"reason"`
-		     } `json:"failures"`
-	     } `json:"_shards"`
+		Total      int `json:"total"`
+		Successful int `json:"successful"`
+		Failed     int `json:"failed"`
+		Failures   []struct {
+			Shard  int         `json:"shard"`
+			Index  string      `json:"index"`
+			Status int         `json:"status"`
+			Reason interface{} `json:"reason"`
+		} `json:"failures"`
+	} `json:"_shards"`
 }
 
-type ClusterVersion struct{
-	Name   string `json:"name"`
-	ClusterName   string `json:"cluster_name"`
+type ClusterVersion struct {
+	Name        string `json:"name"`
+	ClusterName string `json:"cluster_name"`
 	Version     struct {
-			 Number string    `json:"number"`
-			 LuceneVersion string    `json:"lucene_version"`
-		 } `json:"version"`
+		Number        string `json:"number"`
+		LuceneVersion string `json:"lucene_version"`
+	} `json:"version"`
 }
 
 type ClusterHealth struct {
@@ -63,46 +63,42 @@ type ClusterHealth struct {
 	Status string `json:"status"`
 }
 
-type Migrator struct{
-
-	FlushLock       sync.Mutex
-	DocChan         chan map[string]interface{}
-	SourceESAPI     ESAPI
-	TargetESAPI     ESAPI
-	SourceAuth      *Auth
-	TargetAuth      *Auth
-	Config 		*Config
+type Migrator struct {
+	FlushLock   sync.Mutex
+	DocChan     chan map[string]interface{}
+	SourceESAPI ESAPI
+	TargetESAPI ESAPI
+	SourceAuth  *Auth
+	TargetAuth  *Auth
+	Config      *Config
 }
 
-
 type Config struct {
-
 	// config options
-	SourceEs        string `short:"s" long:"source"  description:"source elasticsearch instance, ie: http://localhost:9200"`
-	Query        string `short:"q" long:"query"  description:"query against source elasticsearch instance, filter data before migrate, ie: name:medcl"`
-	TargetEs        string `short:"d" long:"dest"    description:"destination elasticsearch instance, ie: http://localhost:9201"`
-	SourceEsAuthStr string `short:"m" long:"source_auth"  description:"basic auth of source elasticsearch instance, ie: user:pass"`
-	TargetEsAuthStr  string `short:"n" long:"dest_auth"  description:"basic auth of target elasticsearch instance, ie: user:pass"`
+	SourceEs          string `short:"s" long:"source"  description:"source elasticsearch instance, ie: http://localhost:9200"`
+	Query             string `short:"q" long:"query"  description:"query against source elasticsearch instance, filter data before migrate, ie: name:medcl"`
+	TargetEs          string `short:"d" long:"dest"    description:"destination elasticsearch instance, ie: http://localhost:9201"`
+	SourceEsAuthStr   string `short:"m" long:"source_auth"  description:"basic auth of source elasticsearch instance, ie: user:pass"`
+	TargetEsAuthStr   string `short:"n" long:"dest_auth"  description:"basic auth of target elasticsearch instance, ie: user:pass"`
 	DocBufferCount    int    `short:"c" long:"count"   description:"number of documents at a time: ie \"size\" in the scroll request" default:"10000"`
 	Workers           int    `short:"w" long:"workers" description:"concurrency number for bulk workers" default:"1"`
 	BulkSizeInMB      int    `short:"b" long:"bulk_size" description:"bulk size in MB" default:"5"`
 	ScrollTime        string `short:"t" long:"time"    description:"scroll time" default:"1m"`
 	ScrollSliceSize   int    `long:"sliced_scroll_size"    description:"size of sliced scroll, to make it work, the size should be > 1" default:"1"`
-	RecreateIndex     bool      `short:"f" long:"force"   description:"delete destination index before copying" default:"false"`
+	RecreateIndex     bool   `short:"f" long:"force"   description:"delete destination index before copying" `
 	CopyAllIndexes    bool   `short:"a" long:"all"     description:"copy indexes starting with . and _"`
-	CopyIndexSettings bool   `long:"copy_settings"          description:"copy index settings from source" default:"false"`
-	CopyIndexMappings bool   `long:"copy_mappings"          description:"copy index mappings from source" default:"false"`
+	CopyIndexSettings bool   `long:"copy_settings"          description:"copy index settings from source" `
+	CopyIndexMappings bool   `long:"copy_mappings"          description:"copy index mappings from source" `
 	ShardsCount       int    `long:"shards"            description:"set a number of shards on newly created indexes"`
 	SourceIndexNames  string `short:"x" long:"src_indexes" description:"indexes name to copy,support regex and comma separated list" default:"_all"`
 	TargetIndexName   string `short:"y" long:"dest_index" description:"indexes name to save, allow only one indexname, original indexname will be used if not specified" default:""`
 	WaitForGreen      bool   `long:"green"             description:"wait for both hosts cluster status to be green before dump. otherwise yellow is okay"`
 	LogLevel          string `short:"v" long:"log"            description:"setting log level,options:trace,debug,info,warn,error"  default:"INFO"`
-	DumpOutFile       string  `short:"o" long:"output_file"            description:"output documents of source index into local file" `
-	DumpInputFile     string  `short:"i" long:"input_file"            description:"indexing from local dump file" `
-	SourceProxy       string    `long:"source_proxy"            description:"set proxy to source http connections, ie: http://127.0.0.1:8080"`
-	TargetProxy       string    `long:"dest_proxy"            description:"set proxy to target http connections, ie: http://127.0.0.1:8080"`
-	Refresh           bool      `long:"refresh"                 description:"refresh after migration finished"`
-
+	DumpOutFile       string `short:"o" long:"output_file"            description:"output documents of source index into local file" `
+	DumpInputFile     string `short:"i" long:"input_file"            description:"indexing from local dump file" `
+	SourceProxy       string `long:"source_proxy"            description:"set proxy to source http connections, ie: http://127.0.0.1:8080"`
+	TargetProxy       string `long:"dest_proxy"            description:"set proxy to target http connections, ie: http://127.0.0.1:8080"`
+	Refresh           bool   `long:"refresh"                 description:"refresh after migration finished"`
 }
 
 type Auth struct {
--- http.go
+++ http.go
@@ -17,24 +17,25 @@ limitations under the License.
 package main
 
 import (
-	"net/http"
-	"github.com/parnurzeal/gorequest"
-	log "github.com/cihub/seelog"
-	"io/ioutil"
-	"errors"
 	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
 	"net/url"
+
+	log "github.com/cihub/seelog"
+	"github.com/parnurzeal/gorequest"
 )
 
-func Get(url string,auth *Auth,proxy string) (*http.Response, string, []error) {
+func Get(url string, auth *Auth, proxy string) (*http.Response, string, []error) {
 	request := gorequest.New()
-	if(auth!=nil){
-		request.SetBasicAuth(auth.User,auth.Pass)
+	if auth != nil {
+		request.SetBasicAuth(auth.User, auth.Pass)
 	}
 
-	request.Header["Content-Type"]= "application/json"
+	request.Header.Set("Content-Type", "application/json")
 
-	if(len(proxy)>0){
+	if len(proxy) > 0 {
 		request.Proxy(proxy)
 	}
 
@@ -43,28 +44,28 @@ func Get(url string,auth *Auth,proxy string) (*http.Response, string, []error) {
 
 }
 
-func Post(url string,auth *Auth, body string,proxy string)(*http.Response, string, []error)  {
+func Post(url string, auth *Auth, body string, proxy string) (*http.Response, string, []error) {
 	request := gorequest.New()
-	if(auth!=nil){
-		request.SetBasicAuth(auth.User,auth.Pass)
+	if auth != nil {
+		request.SetBasicAuth(auth.User, auth.Pass)
 	}
 
-	request.Header["Content-Type"]= "application/json"
+	request.Header.Set("Content-Type", "application/json")
 
-	if(len(proxy)>0){
+	if len(proxy) > 0 {
 		request.Proxy(proxy)
 	}
 
 	request.Post(url)
 
-	if(len(body)>0){
+	if len(body) > 0 {
 		request.Send(body)
 	}
 
 	return request.End()
 }
 
-func newDeleteRequest(client *http.Client,method, urlStr string) (*http.Request, error) {
+func newDeleteRequest(client *http.Client, method, urlStr string) (*http.Request, error) {
 	if method == "" {
 		// We document that "" means "GET" for Request.Method, and people have
 		// relied on that from NewRequest, so keep that working.
@@ -87,57 +88,55 @@ func newDeleteRequest(client *http.Client,method, urlStr string) (*http.Request,
 	return req, nil
 }
 
-func Request(method string,r string,auth *Auth,body *bytes.Buffer,proxy string)(string,error)  {
+func Request(method string, r string, auth *Auth, body *bytes.Buffer, proxy string) (string, error) {
 
 	var client *http.Client
 	client = &http.Client{}
-	if(len(proxy)>0){
+	if len(proxy) > 0 {
 		proxyURL, err := url.Parse(proxy)
-		if(err!=nil){
+		if err != nil {
 			log.Error(err)
-		}else{
+		} else {
 			transport := &http.Transport{Proxy: http.ProxyURL(proxyURL)}
 			client = &http.Client{Transport: transport}
 		}
 	}
 
-
 	var reqest *http.Request
-	if(body!=nil){
-		reqest, _ =http.NewRequest(method,r,body)
-	}else{
-		reqest, _ = newDeleteRequest(client,method,r)
+	if body != nil {
+		reqest, _ = http.NewRequest(method, r, body)
+	} else {
+		reqest, _ = newDeleteRequest(client, method, r)
 	}
-	if(auth!=nil){
-		reqest.SetBasicAuth(auth.User,auth.Pass)
+	if auth != nil {
+		reqest.SetBasicAuth(auth.User, auth.Pass)
 	}
 
 	reqest.Header.Set("Content-Type", "application/json")
 
-
-	resp,errs := client.Do(reqest)
+	resp, errs := client.Do(reqest)
 	if errs != nil {
 		log.Error(errs)
-		return "",errs
+		return "", errs
 	}
 
 	if resp.StatusCode != 200 {
 		b, _ := ioutil.ReadAll(resp.Body)
-		return "",errors.New("server error: "+string(b))
+		return "", errors.New("server error: " + string(b))
 	}
 
-	respBody,err:=ioutil.ReadAll(resp.Body)
+	respBody, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
 		log.Error(err)
-		return string(respBody),err
+		return string(respBody), err
 	}
 
-	log.Trace(r,string(respBody))
+	log.Trace(r, string(respBody))
 
 	if err != nil {
-		return string(respBody),err
+		return string(respBody), err
 	}
 	defer resp.Body.Close()
-	return string(respBody),nil
+	return string(respBody), nil
 }
